4d54ed489790cfcd0e7ceae526e5ac654ba9423c
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-04-10 15:19:49 -0600

    district repo iteration 0 function

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,8 +1,10 @@
 require './lib/district'
+require 'csv'
+require 'pry'
 
 class DistrictRepository
   attr_reader :districts
-  
+
   def initialize
     @districts = {}
   end
@@ -10,4 +12,41 @@ class DistrictRepository
   def add_district(district)
     @districts[district.name] = district
   end
+
+  def load_data(args)
+    contents = CSV.open args[:enrollment][:kindergarten], headers: true, header_converters: :symbol
+    district_list = map_individual_districts(contents)
+    create_district_objects(district_list)
+  end
+
+  def find_by_name(name)
+    name.upcase!
+    if !@districts.keys.include?(name)
+      nil
+    else
+      @districts[name]
+    end
+  end
+
+  def find_all_matching(name)
+    @districts.keys.select do |districts|
+      name == districts[0..(name.length - 1)]
+    end
+  end
+
+private
+
+  def create_district_objects(district_list)
+    district_list.each do |district|
+      add_district(District.new(:name => district.upcase))
+    end
+  end
+
+  def map_individual_districts(contents)
+    contents = contents.map do |row|
+      row[:location]
+    end.uniq!
+  end
+
+
 end
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -6,16 +6,38 @@ class DistrictRepositoryTest < Minitest::Test
     dr = DistrictRepository.new
     assert_instance_of DistrictRepository, dr
   end
+  
   def test_it_can_create_district_hash
     dr = DistrictRepository.new
     dr.add_district(District.new(:name => "ACADEMY 20"))
     assert_instance_of District, dr.districts["ACADEMY 20"]
   end
 
-  def method_name
+  def test_it_can_parse_districts_from_csv
     dr = DistrictRepository.new
     dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
-    expected = ''
-    actual = dr.keys.count
+    expected = 181
+    actual = dr.districts.keys.count
+    assert_equal expected, actual
+  end
+
+  def test_it_can_find_by_name
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    actual = dr.find_by_name("academy 20").name
+    expected = 'ACADEMY 20'
+    assert_equal expected, actual
+    actual = dr.find_by_name("Colorado").name
+    expected = 'COLORADO'
+    assert_equal expected, actual
+    assert_nil dr.find_by_name("Millburn")
+  end
+
+  def test_if_can_find_all_matching
+    dr = DistrictRepository.new
+    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"}})
+    actual = dr.find_all_matching("AG")
+    expected = ["AGATE 300", "AGUILAR REORGANIZED 6"]
+    assert_equal expected, actual
   end
 end
