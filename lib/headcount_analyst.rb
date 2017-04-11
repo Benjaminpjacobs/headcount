4461eb6cc41a8d35e987d7f08b5155e4b45991cb
Benjamin Ross <benjaminross1001@gmail.com>
2017-04-11 08:47:47 -0600

    small headcount refactor of district kindergarten participation

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -12,21 +12,22 @@ class HeadcountAnalyst
   end
 
   def kindergarten_participation_rate_variation_trend(comparison, base)
-    comparison = district_repository.find_by_name(comparison)
-    comparison_values = comparison.enrollments.kindergarten_participation_by_year
     base = base[:against]
-    base = district_repository.find_by_name(base)
-    base_values = base.enrollments.kindergarten_participation_by_year
+    comparison_values = district_kindergarten_participation(comparison)
+    base_values = district_kindergarten_participation(base)
     comparison_values.merge(base_values) { |key, c_value, b_value| (c_value/b_value).round(3)}.sort.to_h
   end
 
   private
 
   def generate_yearly_statistical_average(district)
-    district = district_repository.find_by_name(district)
-    district_values = district.enrollments.kindergarten_participation_by_year.values
+    district_values = district_kindergarten_participation(district).values
     district_stat = (district_values.inject(&:+)/district_values.length).round(3)
   end
 
+  def district_kindergarten_participation(district)
+    district = district_repository.find_by_name(district)
+    district.enrollments.kindergarten_participation_by_year
+  end
 
 end
