2d318592962d7f9d83507c5c9ba1a478d713d8e8
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-04-12 14:48:54 -0600

    district repo test update

--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -71,9 +71,19 @@ class DistrictRepositoryTest < Minitest::Test
 
   def test_it_can_connect_district_object_to_enrollment_info
     dr = DistrictRepository.new
-    dr.load_data({:enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv"},
-                  :statewide_testing => {:third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv"}
-                })
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv",
+      },
+      :statewide_testing => {
+        :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+        :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+        :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+      }
+    })
     actual = dr.districts["COLORADO"].testing.tests[:third_grade][2011][0]
     expected = [:math, 0.696]
     assert_equal expected, actual
