ba94d036d4d861b8e02bc441278cca208d036d2b
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-04-19 07:13:26 -0600

    test charting all enrollment data

--- test/enrollment_test.rb
+++ test/enrollment_test.rb
@@ -1,5 +1,6 @@
 require_relative 'test_helper'
 require_relative '../lib/enrollment'
+require_relative '../lib/enrollment_repository'
 
 class EnrollmentTest < Minitest::Test
   def test_it_exists
@@ -51,11 +52,22 @@ class EnrollmentTest < Minitest::Test
     assert_equal expected, actual
   end
 
-  # def test_it_can_chart
-  #   e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
-  #   e.add_new_statistics({:name => "ACADEMY 20", :high_school_graduation => {2010 => 0.895, 2011 => 0.895, 2012 => 0.889, 2013 => 0.913, 2014 => 0.898}})
-  #   expected = [[:kindergarten_participation, "Kindergarten participation"], [:high_school_graduation, "High school graduation"]]
-  #   actual = e.chart_data
-  #   assert_equal expected, actual
-  # end
+  def test_it_can_chart
+    e = Enrollment.new({:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}})
+    e.add_new_statistics({:name => "ACADEMY 20", :high_school_graduation => {2010 => 0.895, 2011 => 0.895, 2012 => 0.889, 2013 => 0.913, 2014 => 0.898}})
+    expected = [[:kindergarten_participation, "Kindergarten participation"], [:high_school_graduation, "High school graduation"]]
+    actual = e.chart_all_data
+    assert_equal expected, actual
+  end
+
+  def test_it_can_chart_real_data
+    er = EnrollmentRepository.new
+    er.load_data({ :enrollment => {:kindergarten => "./data/Kindergartners in full-day program.csv", :high_school_graduation => './data/High school graduation rates.csv', :pupil_enrollment => "./data/Pupil enrollment.csv", :special_education => "./data/Special education.csv"}})
+    actual= er.enrollment["COLORADO"].chart_all_data
+    expected = [[:kindergarten, "Kindergarten"], 
+                [:high_school_graduation, "High school graduation"], 
+                [:pupil_enrollment, "Pupil enrollment"], 
+                [:special_education, "Special education"]]
+    assert_equal expected, actual
+  end
 end
